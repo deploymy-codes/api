4f8f2921a3ff21fd6b998a63722b3d69c97a8e56
Anthony Laibe <anthony@laibe.cc>
2014-11-08 15:46:23 +0000

    Add json

--- web/lib/web.rb
+++ web/lib/web.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require "sinatra/reloader"
+require "sinatra/json"
 
 class Web < Sinatra::Base
+  helpers Sinatra::JSON
 
   configure :development do
     register Sinatra::Reloader
