59ded8e25f2621dda2540c2bd4ec1d500be03ed5
Anthony Laibe <anthony@laibe.cc>
2014-11-09 15:24:52 +0000

    Save and find on repo

--- app/lib/repo.rb
+++ app/lib/repo.rb
@@ -1,3 +1,17 @@
 class Repo
   include Facade
+
+  def find(klass, id)
+    raise ArgumentError, "id cannot be nil!" if id.nil?
+    super
+  end
+
+  def save(record)
+    if record.id
+      update record
+    else
+      create record
+    end
+  end
+
 end
