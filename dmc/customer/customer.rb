3519592ff5b0ab724a184cc630f53e222225178e
Anthony Laibe <anthony@laibe.cc>
2014-11-16 22:17:57 +0000

    Add use case find user

--- dmc/customer/customer.rb
+++ dmc/customer/customer.rb
@@ -14,9 +14,11 @@ require_relative 'repos/user_repo'
 require_relative 'repos/adapter/in_memory'
 
 require_relative 'forms/code_form'
+require_relative 'forms/api_key_form'
 
 require_relative 'services/github_service'
 require_relative 'services/github_service/octokit_github_service'
 require_relative 'services/github_service/fake_github_service'
 
 require_relative 'use_cases/find_or_create_user'
+require_relative 'use_cases/find_user'
