cae5d3fa3bbb3772d08051bf43d9837608191248
Anthony Laibe <anthony@laibe.cc>
2014-12-24 07:58:59 +0000

    Add mode in test

--- dmc/config/test.rb
+++ dmc/config/test.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
-Customer::UserRepository.use :active_record
+mode = ENV.fetch 'MODE', 'fast'
+if mode == 'ci'
+  Customer::UserRepository.use :active_record
+else
+  Customer::UserRepository.use :in_memory
+end
 
 Customer::GithubService.use :fake
