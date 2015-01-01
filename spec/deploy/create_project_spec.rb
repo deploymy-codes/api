4bbb7b2c5bb1acc90cb5e31782097eb91dd16ec3
Anthony Laibe <anthony@laibe.cc>
2015-01-01 17:30:20 +0000

    Add missing spec

---
+++ spec/deploy/create_project_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+module Deploy
+  describe 'Create project' do
+
+    it 'create the project based on the user and the remote project' do
+      form = Customer::CodeForm.new code: 'code'
+      user = Customer::FindOrCreateUser.new('github', form).run!
+
+      remote_projects = Customer::ListRemoteProject.new(user).run!
+
+      project = CreateProject.new(user, remote_projects.first).run!
+
+      expect(project.id).to_not be_nil
+      expect(project.name).to be_eql 'rails'
+      expect(project.user_id).to be_eql user.id
+    end
+  end
+end
