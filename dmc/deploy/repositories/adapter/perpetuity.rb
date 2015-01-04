6960f3ee481d33f89e37f91eb9de94098b9f139c
Anthony Laibe <anthony@laibe.cc>
2015-01-04 15:04:18 +0000

    Add perpetuity adapter

--- dmc/deploy/repositories/adapter/perpetuity.rb
+++ dmc/deploy/repositories/adapter/perpetuity.rb
@@ -8,6 +8,12 @@ module Deploy
           mapper.select { |project| project.user_id == selector.user_id }.to_a
         end
 
+        def query_project_with_name_and_user_id(klass, selector)
+          mapper_for(klass).find do |project|
+            project.user_id == selector.user_id && project.name == selector.name
+          end
+        end
+
         def clear
           mapper_for(Deploy::Project).delete_all
         end
