87b6b2a016e53e26165d85cc38190387d884d92d
Anthony Laibe <anthony@laibe.cc>
2015-02-15 21:06:15 +0000

    Call job with id and not object

--- dmc/projects/jobs/clone_job.rb
+++ dmc/projects/jobs/clone_job.rb
@@ -2,7 +2,8 @@ module Projects
   class CloneJob
     include Sidekiq::Worker
 
-    def perform(project)
+    def perform(project_id)
+      project = ProjectRepository.find Project, project_id
       Clone.new(project).run!
     end
 
