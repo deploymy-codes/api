df0ca0051b70dae57e149272369c1632fb1737d7
Anthony Laibe <anthony@laibe.cc>
2015-02-17 13:30:14 +0000

    Refactor deploy

--- dmc/deployments/use_cases/deploy.rb
+++ dmc/deployments/use_cases/deploy.rb
@@ -2,9 +2,17 @@ module Deployments
   class Deploy < Struct.new(:deployment)
 
     def run!
-      environment = Environments::Find.new(deployment.environment_id).run!
+      strategy.deploy deployment
+    end
 
-      "Deployments::#{environment.strategy.classify}Service".constantize.deploy deployment
+    private
+
+    def environment
+      @environment ||= Environments::Find.new(deployment.environment_id).run!
+    end
+
+    def strategy
+      "Deployments::#{environment.strategy.classify}Service".constantize
     end
 
   end
