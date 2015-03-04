a0e49dd85716a6aef76368c2718fae145e5ba2d5
Anthony Laibe <anthony@laibe.cc>
2015-03-04 20:18:43 +0000

    Fix spec

--- spec/support/matchers/paginated.rb
+++ spec/support/matchers/paginated.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :be_paginated do |expected|
   match do |actual|
-    actual.headers['X-LIMIT'] == 100 &&
+    actual.headers['X-LIMIT'] == '100' &&
       actual.headers['X-TOTAL-COUNT'] != '' &&
       actual.headers['Link'] != nil
   end
