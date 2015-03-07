55ab596e7c2bbc942a41d5591651e4cbed45fa8f
Anthony Laibe <anthony@laibe.cc>
2015-03-07 16:48:26 +0000

    Do not create a default environment

--- dmc/projects/use_cases/create.rb
+++ dmc/projects/use_cases/create.rb
@@ -17,24 +17,12 @@ module Projects
       project = Project.build_from user: user, remote_project: remote_project
 
       ProjectRepository.save project
-      add_environment! project
       JobRunner.perform CloneJob, project.id
 
       project
     end
 
     private
-
-    def add_environment!(project)
-      form = Environments::EnvironmentForm.new({
-        name:     Environments::Environment::DEFAULT_NAME,
-        strategy: Environments::Environment::DEFAULT_STRATEGY,
-        heroku_api_key: 'Replace by your api key',
-        heroku_app_name: 'Replace by your app name',
-      })
-
-      Environments::Create.new(project, form).run!
-    end
 
     def validate_project_uniqueness!
       if ListForUser.new(user).run!.map(&:name).include? remote_project.full_name
--- spec/requests/projects/environments_spec.rb
+++ spec/requests/projects/environments_spec.rb
@@ -7,8 +7,9 @@ describe 'List environment' do
     Endpoint::Projects
   end
 
-  let(:user)     { create_user }
-  let!(:project) { create_project user: user }
+  let(:user)         { create_user }
+  let(:project)      { create_project user: user }
+  let!(:environment) { create_environment project: project }
 
   it_behaves_like 'Authenticated', '/rails/rails/environments'
   it_behaves_like 'HasCurrentProject'
@@ -19,7 +20,7 @@ describe 'List environment' do
     expect(last_response.status).to be_eql 200
     json = JSON.parse(last_response.body)
     expect(json).to be_instance_of Array
-    expect(json.first['name']).to be_eql 'default'
+    expect(json.first['name']).to be_eql 'master'
     expect(json.first['strategy']).to be_eql 'heroku'
 
     expect(last_response).to be_paginated
